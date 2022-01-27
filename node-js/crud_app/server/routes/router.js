acd7b9510ac47dfe2cb4e75665e58fe1400e386b
arnab <arnab@selegic.com>
2022-01-27 17:14:52 +0530

    CRUD APPLication

--- node-js/crud_app/server/routes/router.js
+++ node-js/crud_app/server/routes/router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const route = express.Router();
-
-
+const controller = require('../controller/controller');
 const services = require('../services/render')
 /**
  * @description Root Route
@@ -14,6 +13,12 @@ route.get('/add-user',services.add_user);
 
 route.get('/update-user',services.update_user);
 
+
+// API
+route.post('/api/users',controller.create);
+route.get('/api/users',controller.find);
+route.put('/api/users/:id',controller.update);
+route.delete('/api/users/:id',controller.delete);
 
 // export routes
 module.exports = route
